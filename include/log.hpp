3c596bd7fb7d3c4c599d781950276ce34af5a761
rhmvl <ramaabdiansyah03@gmail.com>
2025-12-15 16:03:41 +0700

    Improve log

--- include/log.hpp
+++ include/log.hpp
@@ -7,15 +7,15 @@
 #include <sstream>
 #include <string_view>
 
-#define UNUSED(a) (void)a;
-#define NDEBUG
+#define UNUSED(a) (void) a;
+// #define NDEBUG
 
 class Log
 {
    public:
-    enum class Level
+    enum class Level : uint8_t
     {
-        Trace,
+        Trace = 0,
         Debug,
         Info,
         Warn,
@@ -24,17 +24,62 @@ class Log
     };
 
     static inline bool use_color = true;
-    static inline Level min_level =
+
+    template <typename T>
+    static constexpr std::string_view type_name()
+    {
+        return detail::extract(detail::wrapped_type_name<T>());
+    }
+
 #ifdef NDEBUG
-        Level::Info;
+    static inline Level min_level = Level::Info;
 #else
-        Level::Trace;
+    static inline Level min_level = Level::Trace;
 #endif
 
    private:
     static inline std::mutex write_mutex;
 
-    static const std::string_view level_name(Level lvl)
+    struct detail
+    {
+        template <typename T>
+        static constexpr std::string_view wrapped_type_name()
+        {
+#if defined(__clang__)
+            return __PRETTY_FUNCTION__;
+#elif defined(__GNUC__)
+            return __PRETTY_FUNCTION__;
+#elif defined(_MSC_VER)
+            return __FUNCSIG__;
+#else
+            return "Unsupported compiler";
+#endif
+        }
+
+        static constexpr std::string_view extract(std::string_view wrapped)
+        {
+#if defined(__clang__)
+            // "... [T = MyType]"
+            auto start = wrapped.find("T = ") + 4;
+            auto end = wrapped.rfind(']');
+            return wrapped.substr(start, end - start);
+
+#elif defined(__GNUC__)
+            // "... [with T = MyType]"
+            auto start = wrapped.find("T = ") + 4;
+            auto end = wrapped.rfind(']');
+            return wrapped.substr(start, end - start);
+
+#elif defined(_MSC_VER)
+            // "... wrapped_type_name<MyType>(void)"
+            auto start = wrapped.find("wrapped_type_name<") + 18;
+            auto end = wrapped.find(">(void)");
+            return wrapped.substr(start, end - start);
+#endif
+        }
+    };
+
+    static constexpr std::string_view level_name(Level lvl) noexcept
     {
         switch (lvl)
         {
@@ -54,7 +99,7 @@ class Log
         return "UNKWN";
     }
 
-    static const char* level_color(Level lvl)
+    static const char* level_color(Level lvl) noexcept
     {
         if (!use_color) return "";
         switch (lvl)
@@ -75,59 +120,61 @@ class Log
         return "";
     }
 
-    static void reset_color()
+    static std::tm local_time(std::time_t t)
     {
-        if (use_color) std::cout << "\033[0m";
+#if defined(_WIN32)
+        std::tm tm{};
+        localtime_s(&tm, &t);
+        return tm;
+#else
+        std::tm tm{};
+        localtime_r(&t, &tm);
+        return tm;
+#endif
     }
 
    public:
     template <typename... Args>
     static void write(Level lvl, const std::source_location& loc, Args&&... args)
     {
-        if (lvl < min_level) return;
+        if (static_cast<uint8_t>(lvl) < static_cast<uint8_t>(min_level)) return;
 
-        std::lock_guard<std::mutex> lock(write_mutex);
+        std::lock_guard lock(write_mutex);
 
         // timestamp
-        auto now = std::chrono::system_clock::now();
-        auto t = std::chrono::system_clock::to_time_t(now);
-        auto tm = *std::localtime(&t);
+        const auto now = std::chrono::system_clock::now();
+        const auto t = std::chrono::system_clock::to_time_t(now);
+        const auto tm = local_time(t);
 
-        std::ostringstream timestamp;
-        timestamp << std::put_time(&tm, "%H:%M:%S");
-
-        // message
         std::ostringstream msg;
-        (msg << ... << args);
+        (msg << ... << std::forward<Args>(args));
 
-        // output
-        std::cout << level_color(lvl) << "[" << timestamp.str() << "] " << level_name(lvl) << "\033[0m" << " " << msg.str();
+        std::cout << level_color(lvl) << "[" << std::put_time(&tm, "%H:%M:%S") << "] " << level_name(lvl) << "\033[0m "
+                  << msg.str();
 
 #ifndef NDEBUG
-        std::cout << "  \033[90m"
-                  << "(" << loc.file_name() << ":" << loc.line() << " " << loc.function_name() << ")"
-                  << "\033[0m";
+        std::cout << " \033[90m(" << loc.file_name() << ":" << loc.line() << " " << loc.function_name() << ")\033[0m";
 #endif
-        UNUSED(loc);
 
-        reset_color();
-        std::cout << std::endl;
+        std::cout << '\n';
     }
 
     template <typename Ex, typename... Args>
-    static void log_throw(Args&&... args)
+    [[noreturn]] static void log_throw(Args&&... args)
     {
         std::ostringstream oss;
         (oss << ... << std::forward<Args>(args));
-        Log::write(Log::Level::Error, std::source_location::current(), oss.str());
+
+        write(Level::Error, std::source_location::current(), oss.str());
         throw Ex(oss.str());
     }
 };
 
+#define TYPE_NAME(x) Log::type_name<x>()
 #define LOG_TRACE(...) Log::write(Log::Level::Trace, std::source_location::current(), __VA_ARGS__)
 #define LOG_DEBUG(...) Log::write(Log::Level::Debug, std::source_location::current(), __VA_ARGS__)
 #define LOG_INFO(...) Log::write(Log::Level::Info, std::source_location::current(), __VA_ARGS__)
 #define LOG_WARN(...) Log::write(Log::Level::Warn, std::source_location::current(), __VA_ARGS__)
 #define LOG_ERROR(...) Log::write(Log::Level::Error, std::source_location::current(), __VA_ARGS__)
 #define LOG_CRITICAL(...) Log::write(Log::Level::Critical, std::source_location::current(), __VA_ARGS__)
-#define LOG_THROW(exType, ...) Log::log_throw<exType>(__VA_ARGS__)
+#define LOG_THROW(type, ...) Log::log_throw<type>(__VA_ARGS__)
