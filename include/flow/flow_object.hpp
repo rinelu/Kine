427665ea6d71564c7fc09234e277c79325d03812
rhmvl <ramaabdiansyah03@gmail.com>
2025-12-28 15:27:47 +0700

    Implement find, find_all on flow object

--- include/flow/flow_object.hpp
+++ include/flow/flow_object.hpp
@@ -25,15 +25,38 @@ class FlowObject
     virtual void fixed_update(ECS&, float) {}
 
     template <typename T, typename... Args>
-    T* add_child(const std::string& childName, Args&&... args)
+    T* add_child(const std::string& child_name, Args&&... args)
     {
+        static_assert(std::is_base_of_v<FlowObject, T>, "T must derive from FlowObject");
+
         auto child = std::make_unique<T>(std::forward<Args>(args)...);
-        child->name = childName;
+        child->name = child_name;
         child->parent = this;
 
         T* raw = child.get();
         children.push_back(std::move(child));
         return raw;
+    }
+
+    template <typename T>
+    T* find()
+    {
+        static_assert(std::is_base_of_v<FlowObject, T>, "T must derive from FlowObject");
+
+        if (auto* self = dynamic_cast<T*>(this)) return self;
+
+        for (auto& child : children)
+            if (auto* found = child->find<T>()) return found;
+
+        return nullptr;
+    }
+
+    template <typename T>
+    void find_all(std::vector<T*>& out)
+    {
+        static_assert(std::is_base_of_v<FlowObject, T>, "T must derive from FlowObject");
+        if (auto* self = dynamic_cast<T*>(this)) out.push_back(self);
+        for (auto& child : children) child->find_all<T>(out);
     }
 
     FlowObject* find(const std::string& targetName);
